fffe5da76f522791fb7a84d69023c062f3b6f57f
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-09-08 23:07:06 +0530

    organized route handlers

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
-console.log('this shit is working');
+import express from "express";
+import cors from "cors";
+import apiV1 from "./api/v1";
 
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use(cors());
+app.use("/api/v1", apiV1);
+
+app.get("/", (req, res) => {
+  res.send("This shit works!!");
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on port 3000");
+});
