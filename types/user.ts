5bfe8bbebaebc4e921b576bed74472fbe66e8510
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-09-08 23:48:03 +0530

    added singleton postgres instance

--- types/user.ts
+++ types/user.ts
@@ -2,6 +2,6 @@ import z from "zod";
 export const signUpSchema = z.object({
   name: z.string(),
   phone: z.string(),
-  email: z.string(),
+  email: z.email(),
   password: z.string().min(8),
 });
