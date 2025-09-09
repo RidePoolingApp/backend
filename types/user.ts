96931cfbacf01632d785c332a9f5ad4e40bb5323
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-09-09 23:55:49 +0530

    updated types

--- types/user.ts
+++ types/user.ts
@@ -3,5 +3,10 @@ export const signUpSchema = z.object({
   name: z.string(),
   phone: z.string(),
   email: z.email(),
-  password: z.string().min(8),
+  password: z.string().min(8).optional(),
+});
+
+export const loginSchema = z.object({
+  email: z.email(),
+  password: z.string(),
 });
