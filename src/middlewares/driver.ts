79bb9037ed413a6983d0d0bdb52788a7c293092f
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-09-15 11:46:13 +0530

    added driver endpoint middlewares

---
+++ src/middlewares/driver.ts
@@ -0,0 +1,47 @@
+import { type NextFunction, type Response, type Request } from "express";
+import prisma from "../../services/db";
+import { driverDocumentSchema, driverRegisterSchema } from "../../types/driver";
+export const verifyDriver = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const { driverId } = req.params;
+  const driver = await prisma.driver.findFirst({
+    where: {
+      id: driverId,
+    },
+  });
+  if (!driver) {
+    return res.status(401).json({ message: "Unauthorized Request" });
+  }
+  next();
+};
+
+export const verifyDriverRegisterSchema = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const payload = req.body;
+  const verifiedPayload = driverRegisterSchema.safeParse(payload);
+  if (!verifiedPayload.success) {
+    return res.status(400).json({ message: "Invalid Request Body" });
+  }
+  req.body = verifiedPayload.data;
+  next();
+};
+
+export const verifyDriverDocumentSchema = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const payload = req.body;
+  const verifiedPayload = driverDocumentSchema.safeParse(payload);
+  if (!verifiedPayload.success) {
+    return res.status(400).json({ message: "Invalid Request Body" });
+  }
+  req.body = verifiedPayload.data;
+  next();
+};
