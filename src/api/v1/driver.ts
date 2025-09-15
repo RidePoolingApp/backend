1cfdfed6876e365b130e119784a0e7871d1a5ce1
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-09-15 11:46:00 +0530

    added driver endpoints

---
+++ src/api/v1/driver.ts
@@ -0,0 +1,61 @@
+import cors from "cors";
+import express, { type Request, type Response } from "express";
+import prisma from "../../../services/db";
+import {
+  verifyDriver,
+  verifyDriverDocumentSchema,
+  verifyDriverRegisterSchema,
+} from "../../middlewares/driver";
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.post(
+  "/",
+  verifyDriverRegisterSchema,
+  async (req: Request, res: Response) => {
+    const { userId, currentLocationLat, currentLocationLng } = req.body;
+    try {
+      const driver = await prisma.driver.create({
+        data: {
+          userId,
+          currentLocationLat,
+          currentLocationLng,
+        },
+      });
+      if (!driver) {
+        console.log("error creating driver");
+      } else {
+        res.status(201).json({
+          message: "Registered as driver successfully",
+        });
+      }
+    } catch (err) {
+      console.log("failed to register driver  ", err);
+    }
+  },
+);
+
+app.post(
+  "/documents",
+  verifyDriver,
+  verifyDriverDocumentSchema,
+  async (req: Request, res: Response) => {
+    try {
+      const driverDocument = await prisma.driverDocument.createMany({
+        data: req.body,
+      });
+      if (!driverDocument) {
+        return res
+          .status(400)
+          .json({ message: "failed to upload driver document" });
+      }
+      res.status(201).json({
+        message: "uploaded successfully, forwarding for verification",
+      });
+    } catch (err) {
+      console.log("error uploading driver documents", err);
+    }
+  },
+);
+export default app;
