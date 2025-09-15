af1d96266720276270303cb381ed7b5c1b6140df
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-09-15 11:45:06 +0530

    include /driver endpoints

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import cors from "cors";
 import user from "./user";
+import driver from "./driver";
 const app = express();
 
 app.use(express.json());
 app.use("/user", user);
-
+app.use("/driver", driver);
 app.use(cors());
 
 app.get("/", (req, res) => {
