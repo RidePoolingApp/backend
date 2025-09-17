93134585b2b51c575d7eced8b73c7be51313d4f8
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-09-17 18:28:52 +0530

    start socket server

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import cors from "cors";
 import apiV1 from "./api/v1";
-
+import http from "http";
+import SocketService from "../services/socketService";
 const app = express();
 const PORT = 3000;
-
 app.use(express.json());
 
 app.use(cors());
@@ -14,6 +14,26 @@ app.get("/", (req, res) => {
   res.send("This shit works!!");
 });
 
-app.listen(PORT, () => {
-  console.log("Server is running on port 3000");
-});
+const startServer = async () => {
+  // create a new socket service from SocketService class
+  const PORT = 3000;
+
+  const socketService = new SocketService();
+  // create a new http server
+  // set the port
+  const httpServer = http.createServer(app);
+  // attach the socket service to the http server
+  socketService.getIo().attach(httpServer);
+  // listen to the port
+  httpServer.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+  // initialize the socket listeners
+  socketService.initListeners();
+
+  app.get("/", (req, res) => {
+    res.send("This shit works!!");
+  });
+};
+
+startServer();
