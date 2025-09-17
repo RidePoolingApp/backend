7ea477d1e8be78dc9fc4b0e33000156663375cf4
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-09-17 18:29:36 +0530

    verify if user is driver by jwt

--- src/middlewares/driver.ts
+++ src/middlewares/driver.ts
@@ -1,4 +1,5 @@
 import { type NextFunction, type Response, type Request } from "express";
+import jwt from "jsonwebtoken";
 import prisma from "../../services/db";
 import { driverDocumentSchema, driverRegisterSchema } from "../../types/driver";
 export const verifyDriver = async (
@@ -6,16 +7,42 @@ export const verifyDriver = async (
   res: Response,
   next: NextFunction,
 ) => {
-  const { driverId } = req.params;
+  const { authorization } = req.headers;
+  const userId = await verifyUserId(authorization!);
+  if (userId === 0) {
+    console.log("after jwt verify");
+
+    return res
+      .status(401)
+      .json({ message: "Unauthorized Request after jwt verify" });
+  }
   const driver = await prisma.driver.findFirst({
     where: {
-      id: driverId,
+      userId: String(userId),
     },
   });
   if (!driver) {
-    return res.status(401).json({ message: "Unauthorized Request" });
+    console.log("after db call");
+
+    return res
+      .status(401)
+      .json({ message: "Unauthorized Request after db call" });
   }
   next();
+};
+
+const verifyUserId = async (authorization: string) => {
+  const token = authorization.split(" ")[1];
+  if (!token) {
+    return 0;
+  }
+  const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {
+    userId: string;
+  };
+  if (!decoded.userId) {
+    return 0;
+  }
+  return decoded.userId;
 };
 
 export const verifyDriverRegisterSchema = async (
