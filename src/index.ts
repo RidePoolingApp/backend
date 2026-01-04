d6d0516f98a070db1a15972df80dcb2e2aa2d619
shivaraj110 <shivarajchandaragi9@gmail.com>
2026-01-04 13:22:28 +0530

    update the index page

--- src/index.ts
+++ src/index.ts
@@ -2,37 +2,29 @@ import express from "express";
 import cors from "cors";
 import apiV1 from "./api/v1";
 import http from "http";
-//import SocketService from "../services/socketService";
+import SocketService from "../services/socketService";
+
 const app = express();
 app.use(express.json());
-
 app.use(cors());
 app.use("/api/v1", apiV1);
 
-app.get("/", (req, res) => {
-  res.send("This shit works!!");
+app.get("/", (_req, res) => {
+  res.send("WayLink Backend API");
 });
 
 const startServer = async () => {
-  // create a new socket service from SocketService class
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
 
-  //const socketService = new SocketService();
-  // create a new http server
-  // set the port
+  const socketService = new SocketService();
   const httpServer = http.createServer(app);
-  // attach the socket service to the http server
-  //  socketService.getIo().attach(httpServer);
-  // listen to the port
+  socketService.getIo().attach(httpServer);
+
   httpServer.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-  // initialize the socket listeners
-  //socketService.initListeners();
 
-  app.get("/", (req, res) => {
-    res.send("This shit works!!");
-  });
+  socketService.initListeners();
 };
 
 startServer();
